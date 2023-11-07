1970d49d995c3a70932a7d219e02c31422d1688b
ola <atobateleameer@gmail.com>
2023-11-07 21:10:47 +0100

    finishing

--- scripts/myscript.js
+++ scripts/myscript.js
@@ -2,10 +2,32 @@
 
 /* code for image changer */
 
-var dogImage = document.querySelector('img');
 
-dogImage.onclick = function () {
-    'use strict';
+function showSrc() {
+
+  var dogImage = document.querySelector('.img1'); //#id
+  var dsrc = dogImage.getAttribute('src')
+  dogImage.setAttribute('src', "images/Beans%20and%20plantain.jpg.jpeg")
+  //alert(dsrc)
+}
+
+function changeimageOnHover() {
+  // let image =document.querySelector('#${imageld}');
+const img2 = document.querySelector(".img2")
+img2.setAttribute("src", "images/Amala.jpg.jpeg")
+  /*
+   image.addEventListener('mouseover',function(){
+   }); */
+}
+
+function returnimage() {
+    const img2 =document.querySelector(".img2")
+    img2.setAttribute("src", "images/Beans%20and%20plantain.jpg.jpeg")
+}
+// changeimage()
+/*
+dogImage.onfocus = function () {
+
     var myImages = dogImage.getAttribute('src');
     if (myImages === 'images/puppy.jpg') {
         dogImage.setAttribute('src', 'images/dog.jpg');
@@ -20,7 +42,7 @@ var nameButton = document.querySelector('button');
 var myHeading = document. querySelector( 'h1' );
 
 function setUserName() {
-      'use strict ;
+      //'use strict' ;
     var myName = window.prompt('Please enter your name.');
     localStorage.setItem( 'name', myName);
     myHeading.textContent = 'Have a nice day, ' + myName;
@@ -32,7 +54,7 @@ if (!localStorage.getItem('name')) {
     myHeading . textContent = 'Have a nice day, ' + storedName; 
 }
 nameButton.onclick = function () {
-    'use strict ;
+   // 'use strict ;
     setUserName ( ) ;
 };
-	
+*/	
